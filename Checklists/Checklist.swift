fb59711ce77509c4c0957e088722792b9a37e236
tongcc8 <tongcc8@gmail.com>
2017-08-11 16:07:51 +0800

    Page 155

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by chentou on 11/8/2017.
+//  Copyright © 2017 uocean. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
